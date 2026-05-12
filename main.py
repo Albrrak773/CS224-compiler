a99bce9b80c2e35d22f108661debabe281b877da
Albrrak773 <Albrrak773@gmail.com>
2026-05-12 20:54:23 +0300

    Phase 4 - files and Intermediate Language

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 """
-Phase 1: A compiler that translates from infix input to postfix output
-Phase 2: clear whitespace and newlines from the input
-Phase 3: Allow Numbers instead of just single digits
-
+Phase 1: A compiler that translates from infix input to postfix output.
+Phase 2: clear whitespace and newlines from the input.
+Phase 3: Allow Numbers instead of just single digits.
+Phase 4: input files and Intermediate Language.
 Grammar:
     expr  -> term rest
     rest  -> + term rest | - term rest | ε
     term  -> NUM { print(NUM.value)  }
 """
 
+import enum
 import re
 
 # region ── Global State ──────────────────────────────────────────────
-input_text0 = "44  +    3      3\n-5+1"
-input_text0 = input_text0.replace(" ", "").replace("\t", "").replace("\n", "")
-input_text = re.split(r"(\W+)", input_text0)
+input_file = open("file.exp", "r", encoding="utf-8")
+output_file = open("file.obj", "w", encoding="utf-8")
+il_file = open("file.il", "w", encoding="utf-8")
+error_file = open("file.err", "w", encoding="utf-8")
+
+input_text_raw = input_file.read()
+input_text_raw = input_text_raw.replace(" ", "").replace("\n", "")
+input_text = re.split(r"(\W+)", input_text_raw)
 print(f"Input: {input_text}")
 input_index = 0
 lookahead = ""
@@ -41,9 +47,28 @@ def lexan():
 
 
 # region ─── Utilities ─────────────────────────────────────────────────
+
+
 def error(reason: str | None = None):
+    error_file.write(f"syntax error: {reason}")
     print(f"\n\033[31msyntax error\033[0m\n{reason}")
     exit(1)
+
+
+class colors(enum.Enum):
+    RED = "\033[31m"
+    CYAN = "\033[36m"
+    END = "\033[0m"
+
+def print_and_write(input: str, color: colors | None = None):
+    if color:
+        print(f"{color.value}{input}{colors.END.value}", end=" ")
+    else:
+        print(input, end=" ")
+    output_file.write(input + " ")
+
+def push_il(input: str):
+    il_file.write("push " + input + "\n")
 
 
 def match(t):
@@ -62,7 +87,8 @@ def term():
     term -> NUM { print(NUM.value) }
     """
     if lookahead == "NUM":
-        print(tokenval, end=" ")
+        print_and_write(str(tokenval))
+        push_il(str(tokenval))
         match(lookahead)
     else:
         error(f"Expected '{lookahead}' to be a NUM")
@@ -77,19 +103,21 @@ def rest():
     if lookahead == "+":
         match(lookahead)
         term()
-        print("+", end=" ")
+        print_and_write("+", colors.CYAN)
+        il_file.write("pop r1\npop r2\nadd r2, r1\npush r2\n")
         rest()
     elif lookahead == "-":
         match(lookahead)
         term()
-        print("-", end=" ")
+        print_and_write("-", colors.CYAN)
+        il_file.write("pop r1\npop r2\nsub r2, r1\npush r2\n")
         rest()
     elif lookahead == "EOF":
         match(lookahead)
-        print("\033[31mEOF\033[0m")
+        print_and_write("EOF", colors.RED)
     # we allow ε so we shouldn't error here?
-    # else:
-    #     error(f"Expected '{lookahead}' to be one of: +, - or EOF")
+    else:
+        error(f"Expected '{lookahead}' to be one of: +, - or EOF")
 
 
 # expr -> term rest
---
+++ phase-specification.md
@@ -0,0 +1,7 @@
+# Phase 4:
+input file, output file and error file:
+In this phase the changes are made only for the input, output and error files instad of
+using the keyboard and the screen.
+We add a new function called init() to read from the inputfile and to initilaize settings at the start of the compiler.
+Update term() and rest() to write into outputfile and error to write into errorfile
+Translate to Intermediate Language.
